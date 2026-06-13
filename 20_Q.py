202d7e1ac32060a0394bba435b67f884d99d8055
Jake Rothacker <jakerothacker@gmail.com>
2026-06-13 13:05:04 -0400

    Made 20_Q,py and created cat and dog dictionaries

---
+++ 20_Q.py
@@ -0,0 +1,21 @@
+cat: dict[str,bool] = {
+    "is it physical": True,
+    "is it ananimal": True,
+    "does it have four legs": True,
+    "is it a plant": False,
+    "is it a canine": False,
+    "is it a feline": True,
+    "can it be brown": True
+}
+dog: dict[str,bool] = {
+    "is it physical": True,
+    "is it ananimal": True,
+    "does it have four legs": True,
+    "is it a plant": False,
+    "is it a canine": True,
+    "is it a feline": False,
+    "can it be brown": True
+}
+
+
+
